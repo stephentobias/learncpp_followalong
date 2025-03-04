47b35b5186c9b34cea7f0adec9d780dbbbcac6bb
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-04 13:43:20 -0600

    Respond to Chapter 3, quiz
    The debugging session shows the variable x is not retaining the user-input integer.

---
+++ chapter 3/chapter3_quiz/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+// question 1
+int readNumber()
+{
+	int x{ };
+	std::cout << "Please enter a number: ";
+	std::cin >> x;
+	return x;
+}
+
+void writeAnswer(int x)
+{
+	std::cout << "The sum is: " << x << '\n';
+}
+
+int main()
+{
+	int x{ readNumber() };
+	x = x + readNumber();
+	writeAnswer(x);
+
+	return 0;
+}
