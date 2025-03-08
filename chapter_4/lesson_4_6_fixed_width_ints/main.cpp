0bd62f1fdfc319de3f3ff7ed3cd3c49bfd9716b1
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-08 02:35:24 -0600

    Show sizeof least and fast types

--- chapter_4/lesson_4_6_fixed_width_ints/main.cpp
+++ chapter_4/lesson_4_6_fixed_width_ints/main.cpp
@@ -1,11 +1,15 @@
-#include <cstdint> // for fixed-width integers
+#include <cstdint> // for fast and least types
 #include <iostream>
 
 int main()
 {
-    std::int32_t x { 32767 }; // x is always a 32-bit integer
-    x = x + 1;                // so 32768 will always fit
-    std::cout << x << '\n';
+	std::cout << "least 8:  " << sizeof(std::int_least8_t) * 8 << " bits\n";
+	std::cout << "least 16: " << sizeof(std::int_least16_t) * 8 << " bits\n";
+	std::cout << "least 32: " << sizeof(std::int_least32_t) * 8 << " bits\n";
+	std::cout << '\n';
+	std::cout << "fast 8:  " << sizeof(std::int_fast8_t) * 8 << " bits\n";
+	std::cout << "fast 16: " << sizeof(std::int_fast16_t) * 8 << " bits\n";
+	std::cout << "fast 32: " << sizeof(std::int_fast32_t) * 8 << " bits\n";
 
-    return 0;
+	return 0;
 }
