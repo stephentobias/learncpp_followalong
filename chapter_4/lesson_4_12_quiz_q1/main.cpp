1ed7ab3a5b18dd6d04ff65bc07e95c65f84e9cd2
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-09 18:05:25 -0500

    Change program to use implicit type conversion instead of explicit
    Instead of using static_case<TYPE>(VAR) I use a function to take int parameters
    but use it with a char parameter. The function then returns the parameter making
    no change. The fact the function only takes int params causes it to implicitly
    type cast the char input as an int.

--- chapter_4/lesson_4_12_quiz_q1/main.cpp
+++ chapter_4/lesson_4_12_quiz_q1/main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 
+int changeInputInt(int x);
+
 int main()
 {
     std::cout << "Enter a single character: ";
     char user_input{ };
     std::cin >> user_input;
-    int user_inp_int{ static_cast<int>(user_input) };
+    int user_inp_int{ changeInputInt(user_input) };
 
     std::cout << "You entered \'" << user_input << "\', which is ASCII code ";
     std::cout << user_inp_int << ".\n";
 
 
     return 0;
+}
+
+int changeInputInt(int x)
+{
+    return x;
 }
