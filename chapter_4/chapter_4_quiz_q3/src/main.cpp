41e49bf93716c987cc10b969c2b5054ea3d9bd9e
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-11 16:00:06 -0500

    Add comments to Ch4 final quiz, q3 code

--- chapter_4/chapter_4_quiz_q3/src/main.cpp
+++ chapter_4/chapter_4_quiz_q3/src/main.cpp
@@ -6,11 +6,12 @@ void printResult(double fall_seconds, double tower_height);
 
 int main()
 {
-	double tower_height{ };
-	
+	// Get height of tower from user	
+	double tower_height{ };	
 	std::cout << "Enter the height of the tower in meters: ";
 	std::cin >> tower_height;
 	
+	// Print the height of the ball at the given seconds
 	printResult(0.0, tower_height);
 	printResult(1.0, tower_height);
 	printResult(2.0, tower_height);
@@ -22,17 +23,27 @@ int main()
 	return 0;
 }
 
+// This function returns the current height of the ball as it falls by first: 
+// 1) calculating distance fallen = gravity_constant * x_seconds^2 / 2
+// 2) then subtractive the distance fallen from the tower height given by the user.
 double calcCurrentHeight(double fall_seconds, double tower_height)
 {
 	double gravity_constant { 9.8 };
 	return tower_height - (gravity_constant * fall_seconds * fall_seconds / 2.0);
 }
 
+// This function test if result is negative since a true would mean the 
+// ball is underneath the ground
 bool isNegative(double height)
 {
 	return height < 0;
 }
 
+// This is the function that performs all the work
+// It first calculates the height of the ball at x seconds then checks if
+// it is underneath the ground by checking if negative. Then prints the 
+// correct message.
+// **This may need to be broken up even further since the name is misleading
 void printResult(double fall_seconds, double tower_height)
 {
 	double height { calcCurrentHeight(fall_seconds, tower_height) };
