2c7dca4018b87d449e847ed5841a247edf62dc40
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-18 18:15:09 -0500

    Last attempt at using string and string_view literals without the namespace
    Per the tip in lesson 5.7, "The “s” suffix lives in the namespace
    std::literals::string_literals". I added the "...::literals::..." part
    of the scopeand I still got the same error.

--- chapter_5/lesson_5_08_examples/src/string_view_literals.cpp
+++ chapter_5/lesson_5_08_examples/src/string_view_literals.cpp
@@ -8,8 +8,8 @@ int main()
     // using namespace std::string_view_literals; // access the sv suffix
 
     std::cout << "foo\n";   // no suffix is a C-style string literal
-    std::cout << "goo\n"std::string::s;  // s suffix is a std::string literal
-    std::cout << "moo\n"std::string_view::sv; // sv suffix is a std::string_view literal
+    std::cout << "goo\n"std::literals::string_literals::s;  // s suffix is a std::string literal
+    std::cout << "moo\n"std::literals::string_view_literals::sv; // sv suffix is a std::string_view literal
 
     return 0;
 }
