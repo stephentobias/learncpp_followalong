8dfe26a5961d7d699892f648ccbe648acbf621b1
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-18 17:48:37 -0500

    Try using string and string_view literals suffix without using directive
    I was curious if I absolutely had to use a using directive in order to use
    the string and string_view suffixes. I instead try to use the scope resolution
    operator. It did not work but not sure why yet.

--- chapter_5/lesson_5_08_examples/src/string_view_literals.cpp
+++ chapter_5/lesson_5_08_examples/src/string_view_literals.cpp
@@ -4,12 +4,12 @@
 
 int main()
 {
-    using namespace std::string_literals;      // access the s suffix
-    using namespace std::string_view_literals; // access the sv suffix
+    // using namespace std::string_literals;      // access the s suffix
+    // using namespace std::string_view_literals; // access the sv suffix
 
     std::cout << "foo\n";   // no suffix is a C-style string literal
-    std::cout << "goo\n"s;  // s suffix is a std::string literal
-    std::cout << "moo\n"sv; // sv suffix is a std::string_view literal
+    std::cout << "goo\n"std::string_literals::s;  // s suffix is a std::string literal
+    std::cout << "moo\n"std::string_view_literals::sv; // sv suffix is a std::string_view literal
 
     return 0;
 }
