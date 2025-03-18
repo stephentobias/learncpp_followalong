74fc78faa91004a962b4c1a5566911a1b8caf506
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-18 17:45:53 -0500

    Commit literals example as shown in lesson

---
+++ chapter_5/lesson_5_08_examples/src/string_view_literals.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>      // for std::string
+#include <string_view> // for std::string_view
+
+int main()
+{
+    using namespace std::string_literals;      // access the s suffix
+    using namespace std::string_view_literals; // access the sv suffix
+
+    std::cout << "foo\n";   // no suffix is a C-style string literal
+    std::cout << "goo\n"s;  // s suffix is a std::string literal
+    std::cout << "moo\n"sv; // sv suffix is a std::string_view literal
+
+    return 0;
+}
