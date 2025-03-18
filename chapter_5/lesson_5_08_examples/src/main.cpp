12c0729c74a5eb3dfd5e4ef40a2be0ecbbb18304
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-18 16:59:34 -0500

    Experiment with std::string_view initialization and assignment
    The example was trying to illustrate that the original variable is not
    modified when string_view is assigned a different value. This brought
    up the question of whether or not this behaves the same way even if you
    initialize the std::string_view with a literal instead of via a variable.
    
    It was unclear to me if the initialized value was different than the assigned
    value in that it had to exist independent of the string_view but I can see
    now that it does not.

--- chapter_5/lesson_5_08_examples/src/main.cpp
+++ chapter_5/lesson_5_08_examples/src/main.cpp
@@ -4,14 +4,11 @@
 
 int main()
 {
-    std::string name { "Alex" };
-    std::string_view sv { name }; // sv is now viewing name
+    std::string_view sv { "Alex" };
     std::cout << sv << '\n'; // prints Alex
 
     sv = "John"; // sv is now viewing "John" (does not change name)
     std::cout << sv << '\n'; // prints John
-
-    std::cout << name << '\n'; // prints Alex
 
     return 0;
 }
