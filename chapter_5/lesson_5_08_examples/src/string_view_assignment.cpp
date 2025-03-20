7fe55fd0cde69d49dcd4cd4c969cdb3ed246bd3c
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-20 11:14:44 -0500

    Add precise language for clarification

--- chapter_5/lesson_5_08_examples/src/string_view_assignment.cpp
+++ chapter_5/lesson_5_08_examples/src/string_view_assignment.cpp
@@ -1,8 +1,6 @@
-
-
 /*
  * per lesson 5.9 this version of the program should be used with caution.
- * below we initialized variable std::string_view 'sv' with a C-style string but if it had been initialized with a std::string it would create undefined behavior since the std::string object would be temporary. the std::string would need to be passed into a std::string variable first before using it to initialize a std::string_view to ensure it is available during the lifetime of the std::string_view. 
+ * below we initialized variable std::string_view 'sv' with a C-style string but if it had been initialized with a std::string literal it would create undefined behavior since the std::string literal would be temporary. the std::string would need to be passed into a std::string variable first before using it to initialize a std::string_view to ensure it is available during the lifetime of the std::string_view. 
  * 
  * The lesson provided the following warning to summarize the proper/improper use: 
  * "Warning:
