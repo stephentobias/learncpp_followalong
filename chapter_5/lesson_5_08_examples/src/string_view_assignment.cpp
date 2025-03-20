83333bda6733688ed66da0f026a7f363c2f0d882
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-20 11:07:29 -0500

    Add additional information in the comments
    The following lesson (5.9) covered the idea of ownership and improper
    use of std::string_view. It reminded me of this test program I wrote for
    practice and I realized I inadvertently avoided improper use. I wanted to
    record that fact in the program comments in case I return to it.

--- chapter_5/lesson_5_08_examples/src/string_view_assignment.cpp
+++ chapter_5/lesson_5_08_examples/src/string_view_assignment.cpp
@@ -1,6 +1,10 @@
+// per lesson 5.9 this version of the program should be used with caution.
+// below we initialized variable std::string_view 'sv' with a C-style string but if it had been initialized with a std::string it would create undefined behavior since the std::string object would be temporary. the std::string would need to be passed into a std::string variable first before using it to initialize a std::string_view to ensure it is available during the lifetime of the std::string_view. 
+
 #include <iostream>
 #include <string>
 #include <string_view>
+
 
 int main()
 {
