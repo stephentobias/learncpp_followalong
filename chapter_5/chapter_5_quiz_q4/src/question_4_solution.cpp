f5480c9eaaad120900483afb46799b3415752a67
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-24 15:19:49 -0500

    Break up program into smaller functions

--- chapter_5/chapter_5_quiz_q4/src/question_4_solution.cpp
+++ chapter_5/chapter_5_quiz_q4/src/question_4_solution.cpp
@@ -1,42 +1,45 @@
 #include <iostream>
 
+std::string get_name(int num);
+int get_age(std::string_view person);
 std::string_view compare_age(int age1, int age2);
+void printOlder(std::string_view person_1, int age_1, std::string_view person_2, int age_2);
 
 int main()
 {
 	
 	// get person 1
-	// *name
-	std::cout << "Enter the name of person #1: " ;
-	std::string person1{ };
-	std::getline(std::cin >> std::ws, person1);
-	// *age
-	std::cout << "Enter the age of " << person1 << ": ";
-	int age1{ };
-	std::cin >> age1;
+	std::string person1{ get_name(1) };
+	int age1{ get_age(person1) };
 	
 	// get person 2
-	// *name
-	std::cout << "Enter the name of person #2: ";
-	std::string person2{ };
-	std::getline(std::cin >> std::ws, person2);
-	// *age
-	std::cout << "Enter the age of " << person2 << ": ";
-	int age2{ };
-	std::cin >> age2;
+	std::string person2{ get_name(2) };
+	int age2{ get_age(person2) };
 	
 	// compare age
-	std::string_view is1older{ compare_age(age1,age2) };
-	if (is1older == "older")
-		std::cout << person1 << " (age " << age1 << ") is older than " << person2 << " (age " << age2 << ").\n";
-	else if (is1older == "younger")
-		std::cout << person2 << " (age " << age2 << ") is older than " << person1 << " (age " << age1 << ").\n";
-	else
-		std::cout << "Both persons are the same age.\n";
+	printOlder(person1, age1, person2, age2);
 	
 	return 0;
 }
 
+std::string get_name(int num)
+{
+	std::cout << "Enter the name of person #" << num << ": " ;
+	std::string person{ };
+	std::getline(std::cin >> std::ws, person);
+	return person;
+}
+
+int get_age(std::string_view person)
+{
+	// *age
+	std::cout << "Enter the age of " << person << ": ";
+	int age{ };
+	std::cin >> age;
+	return age;
+}
+
+// checks who is older
 std::string_view compare_age(int age1, int age2)
 {
 	if (age1 > age2) 
@@ -44,4 +47,15 @@ std::string_view compare_age(int age1, int age2)
 	else if (age1 < age2)
 		return "younger";
 	return "same";
+}
+
+void printOlder(std::string_view person_1, int age_1, std::string_view person_2, int age_2)
+{
+	std::string_view is1older{ compare_age(age_1,age_2) };
+	if (is1older == "older")
+		std::cout << person_1 << " (age " << age_1 << ") is older than " << person_2 << " (age " << age_2 << ").\n";
+	else if (is1older == "younger")
+		std::cout << person_2 << " (age " << age_2 << ") is older than " << person_1 << " (age " << age_1 << ").\n";
+	else
+		std::cout << "Both persons are the same age.\n";
 }
